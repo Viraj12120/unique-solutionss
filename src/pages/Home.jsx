8d8759001b0238bdb845d886ed46458117f20044
Viraj12120 <disaleviraj17@gmail.com>
2026-05-13 19:01:26 +0530

    updated spell check

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 				<meta charSet="UTF-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>
-					Unique Group - Innovative Jig Manufacturing and Machining Solutions
+					Unique Group - Innovative Jig Manufacturing and Machining Solution
 				</title>
 				<meta
 					name="description"
